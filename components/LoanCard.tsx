eb3c66a57f0b23970e576c308b818c2da83ae789
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-05-24 00:26:35 -0700

    out of limit click

--- components/LoanCard.tsx
+++ components/LoanCard.tsx
@@ -9,27 +9,28 @@ export const LoanCard = ({id, loan, opened, setOpened} : any) => {
 	}
 
 	return (
-		<UnstyledButton
-		key={id}
-			onClick={setOpened}
-		>
-			<div className="h-[8rem] min-w-[320px]  max-w-[600px] w-full mx-auto my-2
-											drop-shadow-md
-											flex">
-				<div className="bg-gray-400 h-full w-[1rem] rounded-l-xl">
-				</div>
-				<div className="bg-white h-full w-full rounded-r-xl p-3 grid grid-cols-4 ">
-					<p className="col-span-1 font-raleway flex justify-start">Nombre: </p>
-					<p className="col-span-3 overflow-hidden flex justify-items-start">{shrinkText(loan.debtorName)}</p>
-					<p className="col-span-2 font-raleway flex justify-items-start">Fecha límite: </p>
-					<p className="col-span-2">{loan.dueDate}</p>
-					<p className="col-span-1 font-raleway">Total: </p>
-					<p className="col-span-1">{loan.amount}</p>
-					<p className="col-span-1 font-raleway">Abonado: </p>
-					<p className="col-span-1">{loan.payments}</p>
-				</div>
-			</div>
-		</UnstyledButton>
+		<div className="min-w-[320px] max-w-[600px] w-full mx-auto">
+			<UnstyledButton
+				key={id}
+				onClick={setOpened}
+				className="h-[8rem] w-full my-2
+												drop-shadow-md
+												flex"
+			>
+					<div className="bg-gray-400 h-full w-[1rem] rounded-l-xl">
+					</div>
+					<div className="bg-white h-full w-full rounded-r-xl p-3 grid grid-cols-4 ">
+						<p className="col-span-1 font-raleway flex justify-start">Nombre: </p>
+						<p className="col-span-3 overflow-hidden flex justify-items-start">{shrinkText(loan.debtorName)}</p>
+						<p className="col-span-2 font-raleway flex justify-items-start">Fecha límite: </p>
+						<p className="col-span-2">{loan.dueDate}</p>
+						<p className="col-span-1 font-raleway">Total: </p>
+						<p className="col-span-1">{loan.amount}</p>
+						<p className="col-span-1 font-raleway">Abonado: </p>
+						<p className="col-span-1">{loan.payments}</p>
+					</div>
+			</UnstyledButton>
+		</div>
 	)
 }
 
